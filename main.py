678f8f5ce3acfc8db66d4386e15446a3088ff2d2
Pavel Dubnyakov <49102209+pauldub04@users.noreply.github.com>
2024-06-09 16:13:49 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,29 +6,36 @@ import win32gui
 import requests
 import shutil
 from PIL import Image
+import os
+
+
+# changeable:
+images_url = 'https://paste.gg/p/egorkaalexxx/521d4ebf4fac45f7be200c78dff60553/files/23437fa118c54b8eb15ba9ec5af405c3/raw'
+opacity = 100
+opacity_step = 20
+width = 400
+width_step = 20
+seconds = 60
+in_left = True
+margin_side = 10
+margin_bottom = 150
+framerate = 5
 
 
 white = (255, 255, 255)
 black = (0, 0, 0)
 current_number = -1
 current_image = None
-total = 0
-
-# changeable:
-images_url = 'https://paste.gg/p/egorkaalexxx/521d4ebf4fac45f7be200c78dff60553/files/23437fa118c54b8eb15ba9ec5af405c3/raw'
-opacity = 100
-opacity_step = 30
-width = 400
-width_step = 20
-seconds = 30
-in_left = True
-margin_side = 10
-margin_bottom = 150
+total_cnt = 0
+local_cnt = 0
+local_loaded = False
+saved_cnt = 0
+need_redraw = True
 
 
 def resize_to(number, filename):
     global width
-    image = Image.open(f'{number}.png')
+    image = Image.open(f'.\\all_images\\{number}.png')
     sizes = image.size
     mult = width/sizes[0]
     new_image = image.resize((round(sizes[0]*mult), round(sizes[1]*mult)))
@@ -43,36 +50,81 @@ def open_file(number):
         return None
 
 
-def load_images():
-    global total
-    response = requests.get(images_url)
-    data = response.text
-    urls = data.strip().split()
+def load_local():
+    global total_cnt, local_cnt, local_loaded
+    if local_loaded:
+        return
+    files = []
+    try:
+        files = os.listdir('.\\local_images\\')
+    except:
+        pass
 
-    total = 0
-    for (idx, url) in enumerate(urls):
+    total_cnt = local_cnt = 0
+    for file in files:
         try:
-            response = requests.get(url, stream=True)
-            with open(f'{idx}.png', 'wb') as out_file:
-                shutil.copyfileobj(response.raw, out_file)
-
-            total += 1
+            image = Image.open(f'.\\local_images\\{file}')
+            image.save(f'.\\all_images\\{total_cnt}.png')
+            total_cnt += 1
+            local_cnt += 1
         except:
             continue
-    print('now', total, 'images')
+
+    local_loaded = True
+    # print('loaded local', local_cnt)
+
+
+def load_from_url():
+    global total_cnt, local_cnt, saved_cnt
+    urls = []
+    try:
+        response = requests.get(images_url)
+        data = response.text
+        urls = data.strip().split()
+    except:
+        pass
+
+    total_cnt = local_cnt
+    saved_cnt = 0
+    for url in urls:
+        try:
+            response = requests.get(url, stream=True)
+            with open(f'.\\all_images\\{total_cnt}.png', 'wb') as out_file:
+                shutil.copyfileobj(response.raw, out_file)
+
+            total_cnt += 1
+            saved_cnt += 1
+        except:
+            continue
+    # print('loaded from url', saved_cnt)
+
+
+def load_images():
+    global need_redraw, total_cnt
+    load_local()
+    load_from_url()
+    # print('now', total_cnt, 'images')
+    need_redraw = True
 
 
 def select_image(delta):
-    global current_number, current_image, total
-    if current_number == -1 and total > 0:
+    global current_number, current_image, total_cnt, need_redraw
+    if current_number == -1 and total_cnt > 0:
+        if delta == 1:
+            current_number = 0
+        else:
+            current_number = total_cnt-1
+    elif current_number+delta == -1:
+        current_number = total_cnt-1
+    elif current_number+delta == total_cnt:
         current_number = 0
-        current_image = open_file(0)
-    elif 0 <= current_number+delta and current_number+delta < total:
+    else:
         current_number += delta
-        current_image = open_file(current_number)
 
+    current_image = open_file(current_number)
     if current_image is None:
         current_number = -1
+    need_redraw = True
 
 
 def blit_with_alpha(target, source, location):
@@ -86,7 +138,7 @@ def blit_with_alpha(target, source, location):
 
 
 def run():
-    global current_image, current_number, width, width_step, seconds, margin_bottom, margin_side, in_left, opacity, opacity_step
+    global current_image, current_number, width, width_step, seconds, margin_bottom, margin_side, in_left, opacity, opacity_step, need_redraw, framerate
     pygame.init()
 
     load_images()
@@ -99,18 +151,23 @@ def run():
     win32gui.SetWindowLong(hwnd, win32con.GWL_EXSTYLE, win32gui.GetWindowLong(hwnd, win32con.GWL_EXSTYLE) | win32con.WS_EX_LAYERED)
     win32gui.SetLayeredWindowAttributes(hwnd, win32api.RGB(*white), 0, win32con.LWA_COLORKEY)
     win32gui.SetWindowPos(hwnd, win32con.HWND_TOPMOST, 0,0,0,0, win32con.SWP_NOMOVE | win32con.SWP_NOSIZE)
+    
+    screen.fill(white)
+    pygame.display.update()
 
     font = pygame.font.Font("Roboto-Regular.ttf", 12)
 
     done = 0
-    while not done:   
+    clock = pygame.time.Clock()
+    while not done:
         for event in pygame.event.get():    
             if event.type == pygame.QUIT:   
                 done = 1
                 break
             if event.type == pygame.USEREVENT:
                 load_images()
-                select_image(0)
+                if current_number != -1:
+                    select_image(0)
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     done = 1
@@ -118,16 +175,23 @@ def run():
                 if event.key in [pygame.K_q, pygame.K_LCTRL]:
                     current_number = -1
                     current_image = None
+                    need_redraw = True
 
                 if event.key == pygame.K_z:
                     in_left = True
+                    need_redraw = True
                 if event.key == pygame.K_x:
                     in_left = False
+                    need_redraw = True
 
                 if event.key == pygame.K_c:
-                    opacity -= opacity_step
+                    if opacity-opacity_step >= 0:
+                        opacity -= opacity_step
+                        need_redraw = True
                 if event.key == pygame.K_v:
-                    opacity += opacity_step
+                    if opacity+opacity_step <= 300:
+                        opacity += opacity_step
+                        need_redraw = True
 
                 if event.key == pygame.K_UP:
                     width += width_step
@@ -141,15 +205,19 @@ def run():
                 if event.key == pygame.K_RIGHT:
                     select_image(+1)
 
+
+        if not need_redraw:
+            continue
+
         screen.fill(white)
 
-        text = font.render(f"{str(current_number+1)}/{str(total)}", True, black)
+        text = font.render(f"{str(current_number+1)}/{str(total_cnt)} ({str(local_cnt)}+{str(saved_cnt)})", True, black)
         text_size = text.get_size()
         text_pos = (0, 0)
         if in_left:
             text_pos = (margin_side, window_size[1]-text_size[1]-margin_bottom)
         else:
-            text_pos = (window_size[0]-text_size[0]-margin_side, window_size[1]-text_size[1]-margin_bottom)
+            text_pos = (window_size[0]-text_size[0]-margin_side-10, window_size[1]-text_size[1]-margin_bottom)
         blit_with_alpha(screen, text, text_pos)
 
         if current_image is not None:
@@ -162,14 +230,17 @@ def run():
             blit_with_alpha(screen, current_image, image_pos)
         
         pygame.display.update()
+        need_redraw = False
+        clock.tick(framerate)
 
     pygame.quit()
 
 
 if __name__ == '__main__':
-    while True:
-        try:
-            run()
-            break
-        except:
-            continue
+    run()
+    # while True:
+    #     try:
+    #         run()
+    #         break
+    #     except:
+    #         continue
